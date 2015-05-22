384c894881ac539bc67a6f487fdc6f9227719002
flipwhatthezip2 <flipwhatthezip2.mc@gmail.com>
2015-05-22 16:00:30 -0400

    Updated chat formatting. Added GameAPI support.

--- src/ml/lasertag/minigame/Core.java
+++ src/ml/lasertag/minigame/Core.java
@@ -13,7 +13,6 @@ package ml.lasertag.minigame;
  */
 
 
-import gameAPI.Flipwhatthezip2.chat.ChatStrings;
 import ml.lasertag.minigame.GameManager.ArenaSelector;
 import ml.lasertag.minigame.GameManager.ArenasFile;
 import ml.lasertag.minigame.Mechanics.LaserGun;
@@ -41,38 +40,25 @@ public class Core extends JavaPlugin {
     public static String warning = "§4§l>> §c";
     public static String warningList = "§4§l> §c";
 
-    public static String joinMessage = ChatStrings.joinMessage;
-    public static String quitMessage = ChatStrings.quitMessage;
+    public static String joinMessage = "§3Join> §7";
+    public static String quitMessage = "§3Quit> §7";
 
-    public static String deathMessage = ChatStrings.deathMessage;
-    public static String killMessage = ChatStrings.killMessage;
+    public static String deathMessage = "§3Death> §7";
+    public static String killMessage = "§3Kill> §7";
 
-    public static String teamMessage = ChatStrings.teamMessage;
-    public static String gameMessage = ChatStrings.gameMessage;
-    public static String statusMessage = ChatStrings.statusMessage;
-    public static String rechargingMessage = ChatStrings.rechargingMessage;
-    public static String powerupMessage = ChatStrings.powerupMessage;
+    public static String teamMessage = "§3Team> §7";
+    public static String gameMessage = "§3Game> §7";
+    public static String statusMessage = "§3Status> §7";
+    public static String rechargingMessage = "§3Recharging> §7";
+    public static String powerupMessage = "§3Powerup> §7";
 
-    public static String infoMessage = ChatStrings.infoMessage;
-    public static String serverMessage = ChatStrings.serverMessage;
-    public static String portalMessage = ChatStrings.portalMessage;
+    public static String infoMessage = "§3Info> §7";
+    public static String serverMessage = "§3Server> §7";
+    public static String portalMessage = "§3Portal> §7";
 
     public void onEnable(){
 
-        ChatStrings.setJoinMessage("§3Join> §7");
-        ChatStrings.setQuitMessage("§3Quit> §7");
-        ChatStrings.setDeathMessage("§3Death> §7");
-        ChatStrings.setKillMessage("§3Kill> §7");
-        ChatStrings.setTeamMessage("§3Team> §7");
-        ChatStrings.setGameMessage("§3Game> §7");
-        ChatStrings.setStatusMessage("§3Status> §7");
-        ChatStrings.setRechargingMessage("§3Recharging> §7");
-        ChatStrings.setPowerupMessage("§3Powerup> §7");
-        ChatStrings.setInfoMessage("§3Info> §7");
-        ChatStrings.setServerMessage("§3Server> §7");
-        ChatStrings.setPortalMessage("§3Portal> §7");
-
-        Bukkit.getServer().getLogger().info(ChatStrings.serverMessage() + "Successfully initialized the minigame 'LaserTag' on server: " + Bukkit.getServerName());
+        Bukkit.getServer().getLogger().info("Server> Successfully initialized the minigame 'LaserTag' on server: " + Bukkit.getServerName());
 
         this.arenasFile = new ArenasFile(this);
         this.arenasFile.loadArenas();
@@ -92,7 +78,7 @@ public class Core extends JavaPlugin {
     }
 
     public void onDisable(){
-        for (World w : Bukkit.getWorlds()){
+        for (World w : Bukkit.getWorlds()) {
             for (Entity e : w.getEntities()){
                 if (e instanceof EnderCrystal) e.remove();
             }
